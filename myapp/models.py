b16c66ccf2cbe0e841b8be4178c6b83eb235aef2
aryan7081 <haryan458@gmail.com>
2024-06-17 14:56:34 +0530

    Updated model fiels and change migration

--- myapp/models.py
+++ myapp/models.py
@@ -14,11 +14,11 @@ class Scheme(models.Model):
     scheme_benefits = models.CharField(max_length=255)
     how_to_avail = models.CharField(max_length=255)
     sponsors = models.CharField(max_length=255)
-    age_from = models.CharField(max_length=255, blank=True, null=True)
-    age_to = models.CharField(max_length=255, blank=True, null=True)
+    lower_age = models.IntegerField(max_length=255, blank=True, null=True)
+    upper_age = models.IntegerField(max_length=255, blank=True, null=True)
     funding = models.CharField(max_length=255)
     scheme_link = models.URLField()
-    json_data = models.JSONField()
+
 
     def __str__(self):
         return self.scheme_name
