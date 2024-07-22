9f6eff75d5d80ac253ce4a120336661592d5a10f
aryan7081 <haryan458@gmail.com>
2024-07-22 20:04:13 +0530

    Added import export functionality  for schemes

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import State, Department, Organisation, Scheme, Beneficiary, SchemeBeneficiary, Benefit, Criteria, Procedure, Document, SchemeDocument, Sponsor, SchemeSponsor, CustomUser, Banner
-
-
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
+from .models import (
+    State, Department, Organisation, Scheme, Beneficiary, SchemeBeneficiary,
+    Benefit, Criteria, Procedure, Document, SchemeDocument, Sponsor,
+    SchemeSponsor, CustomUser, Banner
+)
 
 admin.site.site_header = "Community Empowerment Portal Admin Panel"
 admin.site.site_title = "Admin Portal"
@@ -11,7 +15,6 @@ admin.site.index_title = "Welcome to your Admin Panel"
 admin.site.register(State)
 admin.site.register(Department)
 admin.site.register(Organisation)
-admin.site.register(Scheme)
 admin.site.register(Beneficiary)
 admin.site.register(SchemeBeneficiary)
 admin.site.register(Benefit)
@@ -21,7 +24,6 @@ admin.site.register(Document)
 admin.site.register(SchemeDocument)
 admin.site.register(Sponsor)
 admin.site.register(SchemeSponsor)
-
 
 class CustomUserAdmin(UserAdmin):
     model = CustomUser
@@ -45,6 +47,19 @@ class CustomUserAdmin(UserAdmin):
 admin.site.register(CustomUser, CustomUserAdmin)
 
 @admin.register(Banner)
-class BannerAdmin(admin.ModelAdmin):
+class BannerAdmin(ImportExportModelAdmin):
     list_display = ['title', 'is_active']
     search_fields = ['title']
+
+class SchemeResource(resources.ModelResource):
+    class Meta:
+        model = Scheme
+        fields = ('id', 'title', 'department__department_name', 'introduced_on', 'valid_upto', 'funding_pattern', 'description', 'scheme_link')
+        export_order = ('id', 'title', 'department__department_name', 'introduced_on', 'valid_upto', 'funding_pattern', 'description', 'scheme_link')
+
+@admin.register(Scheme)
+class SchemeAdmin(ImportExportModelAdmin):
+    resource_class = SchemeResource
+    list_display = ('title', 'department', 'introduced_on', 'valid_upto', 'funding_pattern')
+    search_fields = ('title', 'description')
+    list_filter = ('department', 'introduced_on', 'valid_upto', 'funding_pattern')
